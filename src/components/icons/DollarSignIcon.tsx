52bfaa59010558f2b5f9472ccd1ca45f59a888d4
Yaroslav Podkovenko <ypodkovenko@united-internet.de>
2025-11-16 11:45:00 +0100

    Navigation bar and Icons

---
+++ src/components/icons/DollarSignIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+export function DollarSignIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M8 1.33334V14.6667M11.3333 4.00001H6.33333C5.71449 4.00001 5.121 4.24583 4.68342 4.68342C4.24583 5.121 4.00001 5.71449 4.00001 6.33334C4.00001 6.95218 4.24583 7.54567 4.68342 7.98326C5.121 8.42084 5.71449 8.66667 6.33333 8.66667H9.66667C10.2855 8.66667 10.879 8.91249 11.3166 9.35008C11.7542 9.78767 12 10.3812 12 11C12 11.6188 11.7542 12.2123 11.3166 12.6499C10.879 13.0875 10.2855 13.3333 9.66667 13.3333H4.00001"
+                stroke="currentColor"
+                strokeWidth={1.6}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+}
