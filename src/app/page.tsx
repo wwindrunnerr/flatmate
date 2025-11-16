cda932eb1ef63adccb4ffa3c8114bc940a48d272
wwindrunner <simple.csgo.noscope@gmail.com>
2025-11-16 14:55:47 +0100

    implemented basic main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { HomeIcon } from "@/components/icons/HomeIcon";
+import {ProfileIcon} from "@/components/icons/ProfileIcon";
 
 export default function MainPage() {
     // eine Variable, die am Anfang false ist
@@ -15,9 +17,131 @@ export default function MainPage() {
 
     // wenn nicht eingeloggt (!loggedIn), dann Login-Link anzeigen
     return (
-        <div>
-            <h2>Main Page</h2>
-            {!loggedIn && <Link href="/login">Login</Link>}
+        <div className="page">
+
+            <div className="icon-container">
+                <h1 className="title"><HomeIcon className={"home"}></HomeIcon> Hallo User!</h1>
+                <Link href={"/dashboard"}><h1 className="title"><ProfileIcon className={"home"}></ProfileIcon></h1></Link>
+            </div>
+
+            <div className="grid">
+                {/* --------- BUDGET --------- */}
+                <div className="widget">
+                    <h2>Budget</h2>
+                    <div className="row">
+                        <span>Zum Denis</span>
+                        <span>78 €</span>
+                    </div>
+                    <div className="row">
+                        <span>Zum Yaroslav</span>
+                        <span>67 €</span>
+                    </div>
+                    <div className="row">
+                        <span>Von Mykyta</span>
+                        <span>2000 €</span>
+                    </div>
+                </div>
+
+                {/* --------- EINKAUFSLISTEN --------- */}
+                <div className="widget">
+                    <h2>Einkaufslisten</h2>
+                    <ul>
+                        <li><Link href="#">Wocheneinkauf</Link></li>
+                        <li><Link href="#">Putzmittel</Link></li>
+                        <li><Link href="#">Vorräte</Link></li>
+                    </ul>
+                </div>
+
+                {/* --------- PUTZPLAN --------- */}
+                <div className="widget">
+                    <h2>Noch zum putzen</h2>
+                    <div className="row checkbox-row">
+                        <span>Bad</span>
+                        <input type="checkbox" />
+                    </div>
+                    <div className="row checkbox-row">
+                        <span>Küche</span>
+                        <input type="checkbox" defaultChecked />
+                    </div>
+                </div>
+
+                {/* --------- EVENTS --------- */}
+                <div className="widget">
+                    <h2>Events</h2>
+
+                    <div className="row">
+                        <span>Waschtag</span>
+                        <span>morgen</span>
+                    </div>
+
+                    <div className="row">
+                        <span>Hausmeister</span>
+                        <span>in 3 Tagen</span>
+                    </div>
+
+                    <div className="row">
+                        <span>Geburtstag von Denis</span>
+                        <span>10.11.2025</span>
+                    </div>
+                </div>
+            </div>
+
+            <style jsx global>{`
+                .page {
+                    padding: 20px 40px;
+                    font-family: sans-serif;
+                }
+                
+                .icon-container {
+                    display: flex;
+                    gap: 110vh;
+                }
+                
+                .home {
+                    width: 48px;
+                    height: 48px;
+                    
+                }
+
+                .title {
+                    font-size: 48px;
+                    margin-bottom: 20px;
+                    display: flex;
+                    align-items: center;
+                    gap: 10px;
+                }
+
+                .grid {
+                    display: grid;
+                    grid-template-columns: repeat(2, 1fr);
+                    gap: 25px;
+                }
+
+                .widget {
+                    background: #e5e5e5;
+                    border-radius: 15px;
+                    padding: 20px;
+                }
+
+                .widget h2 {
+                    margin-bottom: 15px;
+                    text-decoration: underline;
+                }
+
+                .row {
+                    display: flex;
+                    justify-content: space-between;
+                    margin-bottom: 10px;
+                }
+
+                ul {
+                    padding-left: 18px;
+                }
+                .checkbox-row input {
+                    width: 18px;
+                    height: 18px;
+                }
+            `}</style>
         </div>
     );
 }
---
+++ src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+export function ProfileIcon(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            viewBox="0 0 340 340"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="m169,.5a169,169 0 1,0 2,0zm0,86a76,76 0 11-2,0zM57,287q27-35 67-35h92q40,0 67,35a164,164 0 0,1-226,0"  stroke="currentColor"
+                strokeWidth={1.6}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+
+    );
+}
