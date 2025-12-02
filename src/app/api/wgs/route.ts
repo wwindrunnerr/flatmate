0c3b34a56c1de54a066176399f37410c6dd572a5
denis <wwwwoooolk@gmail.com>
2025-12-02 10:09:27 +0100

    old paths

--- src/app/api/wgs/route.ts
+++ src/app/api/wgs/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-const wgsPath = path.join(process.cwd(), "src/app/api/data/wgs.json");
+const wgsPath = path.join(process.cwd(), "app/api/data/wgs.json");
 
 export async function GET() {
     const wgs = JSON.parse(fs.readFileSync(wgsPath, "utf8"));
